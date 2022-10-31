57f9c2fa6244ecdad75442f1c5ebf26412dc212b
Vyom <vkapur_be20@thapar.edu>
2022-10-31 20:29:31 +0530

    uploading on github

--- models/model.py
+++ models/model.py
@@ -1,15 +1,7 @@
 import pandas as pd
 import pickle
-import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-
-def cleanText(text):
-    text = re.sub(r'@[A-Za-z0-9]', '', text)
-    text = re.sub(r'#[A-Za-z0-9]', '', text)
-    text = re.sub(r'RT[\s]+', '', text)
-    text = re.sub(r'https?:\/\/\S+', '', text)
-    return text
 
 data = pd.read_csv('./datasets/train.csv')
 print(data.head())
