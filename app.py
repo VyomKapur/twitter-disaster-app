89a8cbd5a1f06091b507b1ec6eb64b5a537adb21
Vyom <vkapur_be20@thapar.edu>
2022-11-14 10:10:17 +0530

    Final proj

--- app.py
+++ app.py
@@ -5,24 +5,45 @@ import re
 import twint
 import nest_asyncio
 import os
+from geopy.geocoders import Nominatim
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
-from flask import Flask, redirect, session, url_for, render_template, request, flash, session
+from flask import Flask,session, render_template, request, session
 
 nest_asyncio.apply()
 app = Flask(__name__)
 app.secret_key = '\xf0?a\x9a\\\xff\xd4;\x0c\xcbHi'
-model_mnb = pickle.load(open('./models/model.pkl', 'rb'))
-model_gnb = pickle.load(open('./models/model_g.pkl', 'rb'))
-#model_bert = pickle.load(open('./models/bert.pkl', 'rb'))
-vec = pickle.load(open('./models/vec.pkl', 'rb'))
+model_mnb = pickle.load(open('./models/model_mnb.pkl', 'rb'))
+model_bnb = pickle.load(open('./models/model_bnb.pkl', 'rb'))
+model_svm = pickle.load(open('./models/model_svm.pkl', 'rb'))
+lemmatizer = WordNetLemmatizer()
+
+vectorizer = pickle.load(open('./models/vec.pkl', 'rb'))
+vectorizer1 = pickle.load(open('./models/vec1.pkl', 'rb'))
 
 def cleanText(text):
-    text = re.sub(r'@[A-Za-z0-9]', '', text)
-    text = re.sub(r'#[A-Za-z0-9]', '', text)
-    text = re.sub(r'RT[\s]+', '', text)
     text = re.sub(r'https?:\/\/\S+', '', text)
+    text = re.sub(r'[^\w\s]','',text)
+    text = text.lower()
     return text
 
+def lemmat(text) :
+    lem = []
+    for word in text :
+        lem.append(lemmatizer.lemmatize(word))
+    return lem
+
+def ListToStr(l) :
+    return ' '.join(l)
+
+def get_geocode(location):
+    if location == "Worldwide":
+        return False
+    geo = Nominatim(user_agent="Disaster App").geocode(location)
+    geocode = str(geo.latitude) + ", " + str(geo.longitude) + ",10km"
+    return geocode
 
 @app.route('/')
 def homepage():
@@ -41,6 +62,8 @@ async def get_data():
     os.remove("./datasets/dataset1.json")
     if request.method == 'POST':
         limit = request.form['limit']
+        location = request.form['state']
+        geocode = get_geocode(location)
         print(request.form['model'])
         session['model'] = int(request.form['model'])
         c = twint.Config()
@@ -48,7 +71,10 @@ async def get_data():
         c.Store_json = True
         c.Output = "./datasets/dataset1.json"
         c.Limit = int(limit)
+        if geocode != False:
+            c.Geo = geocode
         session['limit'] = limit
+        session['location'] = location
         twint.run.Search(c)
         data = pd.read_json('./datasets/dataset1.json', lines=True, nrows=int(limit)+2)
     return clean_data(data)
@@ -59,22 +85,29 @@ def clean_data(data):
     data.drop_duplicates(subset=['tweet'], inplace=True)
     original_data = data['tweet']
     data['tweet'] = data['tweet'].apply(cleanText)
-    return predict(list(data['tweet']), list(original_data))
+    data['tweet'] = [word_tokenize(entry) for entry in data['tweet']]
+    data['tweet'] = data['tweet'].apply(lemmat)
+    data['lemmatized_word'] = data['tweet'].apply(lemmat)
+    data["lemmatized_word"] = data.lemmatized_word.apply(ListToStr)
+    return predict(list(data['lemmatized_word']), list(original_data))
 
 @app.route('/predict')
 def predict(data, orginal_data):
     data = np.array(data)
+    
     if session['model'] == 0:
-        output = [1 for i in range(len(data))]
-        #BERT application
-        pass
-    elif session['model'] == 1:
-        x = vec.transform(data)
+        x = vectorizer.transform(data)
         output = model_mnb.predict(x)
+        
+    elif session['model'] == 1:
+        x = vectorizer1.transform(data)
+        output = model_bnb.predict(x)
+
     elif session['model'] == 2:
-        x = vec.transform(data)
-        output = model_gnb.predict(x.toarray())
-    return render_template('result.html', pred_text = orginal_data, pred = output, limit = session['limit'])
+        x = vectorizer.transform(data)
+        output = model_svm.predict(x)
+
+    return render_template('result.html', pred_text = orginal_data, pred = output, limit = session['limit'], location=session['location'])
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models/model.py
+++ models/model.py
@@ -1,24 +1,59 @@
 import pandas as pd
+import re
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import BernoulliNB
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn import svm
 
 data = pd.read_csv('./datasets/train.csv')
 print(data.head())
 
+lemmatizer = WordNetLemmatizer()
+
+def cleanText(text):
+    text = re.sub(r'https?:\/\/\S+', '', text)
+    text = re.sub(r'[^\w\s]','',text)
+    text = text.lower()
+    return text
+
+def lemmat(text) :
+    lem = []
+    for word in text :
+        lem.append(lemmatizer.lemmatize(word))
+    return lem
+
+def ListToStr(l) :
+    return ' '.join(l)
+
 vectorizer = CountVectorizer(stop_words='english')
-x = vectorizer.fit_transform(data['text'])
-pickle.dump(vectorizer, open('./models/vec.pkl', 'wb'))
+vectorizer1 = CountVectorizer(stop_words='english', binary=True)
 
 y = data['target']
 
-model = MultinomialNB()
-model.fit(x,y)
+data['text'] = data['text'].apply(cleanText)
+data['text'] = [word_tokenize(entry) for entry in data['text']]
+data['text'] = data['text'].apply(lemmat)
+data['lemmatized_word'] = data['text'].apply(lemmat)
+data["lemmatized_word"] = data["lemmatized_word"].apply(ListToStr)
 
-model_g = GaussianNB()
-model_g.fit(x.toarray(),y)
+x = vectorizer.fit_transform(data['lemmatized_word'])
+x1 = vectorizer1.fit_transform(data['lemmatized_word'])
 
-pickle.dump(model, open('./models/model.pkl', 'wb'))
-pickle.dump(model_g, open('./models/model_g.pkl', 'wb'))
+pickle.dump(vectorizer1, open('./models/vec1.pkl', 'wb'))
+pickle.dump(vectorizer, open('./models/vec.pkl', 'wb'))
 
+model_mnb = MultinomialNB()
+model_mnb.fit(x,y)
+
+model_bnb= BernoulliNB()
+model_bnb.fit(x1,y)
+
+model_svm = svm.SVC(C = 1.0 , kernel = 'linear' , degree = 4 , gamma = 'auto')
+model_svm.fit(x,y)
+
+pickle.dump(model_mnb, open('./models/model_mnb.pkl', 'wb'))
+pickle.dump(model_bnb, open('./models/model_bnb.pkl', 'wb'))
+pickle.dump(model_svm, open('./models/model_svm.pkl', 'wb'))
